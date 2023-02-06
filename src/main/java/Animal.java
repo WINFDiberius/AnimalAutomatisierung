781946f53c8cc2a9c5d7572a61443236270b1593
dsattler <01010174@edu.campus02.at>
2023-02-06 17:30:21 +0100

    new entry

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class Animal {
+public class Animal {
+
+    private String name;
+
+    private int age;
+
+    private String species;
+
+    private String animalLoud;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getAnimalLoud() {
+        return animalLoud;
+    }
+
+    public void setAnimalLoud(String animalLoud) {
+        this.animalLoud = animalLoud;
+    }
+
+    public Animal(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+
+    public String giveAnimalLoud(){
+        switch (species){
+            case "cat":
+                return "Miau";
+            case "dog":
+                return "Wuff";
+            case "lion":
+                return "Roarr";
+            default:
+                return "Murrrgl, no speak Loud";
+        }
+    }
+
+    public String getNameAndAge(){
+        return name + " | " + age;
+    }
+
+    public String ageCategory(){
+        if(age <= 2){
+            return "Baby Animal";
+        }
+        else if(age <= 4){
+            return "Young Animal";
+        }
+        else
+        {
+            return "Old Animal";
+        }
+    }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,4 +1,74 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AnimalTest {
-  
+
+    Animal dog;
+    Animal cat;
+    Animal lion;
+
+    Animal snake;
+
+    Animal bear;
+
+    @BeforeEach
+    void setUp(){
+        dog = new Animal("Wuffi", 3, "dog");
+        cat = new Animal("Kitty", 8, "cat");
+        lion = new Animal("Simba", 4, "lion");
+        snake = new Animal("Slytherin", 1, "snake");
+        bear = new Animal("Winnie", 10, "bear");
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void giveAnimalLoud() {
+
+        Assertions.assertEquals("Wuff", dog.giveAnimalLoud());
+        Assertions.assertEquals("Miau", cat.giveAnimalLoud());
+        Assertions.assertEquals("Roarr", lion.giveAnimalLoud());
+        Assertions.assertEquals("Murrrgl, no speak Loud", snake.giveAnimalLoud());
+        Assertions.assertEquals("Murrrgl, no speak Loud", bear.giveAnimalLoud());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNameAndAge() {
+
+
+        int wuffialter = 3;
+        int kittyalter = 8;
+        int simbaalter = 4;
+        int slytherinalter = 1;
+        int winniealter = 10;
+
+        String wuffiname = "Wuffi";
+        String kittyname = "Kitty";
+        String simbaname = "Simba";
+        String slytherinname = "Slytherin";
+        String winniename = "Winnie";
+
+        Assertions.assertEquals(wuffiname + " | " + wuffialter, dog.getNameAndAge());
+        Assertions.assertEquals(kittyname + " | " + kittyalter, cat.getNameAndAge());
+        Assertions.assertEquals(simbaname + " | " + simbaalter, lion.getNameAndAge());
+        Assertions.assertEquals(slytherinname + " | " + slytherinalter, snake.getNameAndAge());
+        Assertions.assertEquals(winniename + " | " + winniealter, bear.getNameAndAge());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void ageCategory() {
+
+        String babyAnimal = "Baby Animal";
+        String youngAnimal = "Young Animal";
+        String oldAnimal = "Old Animal";
+
+        Assertions.assertEquals(youngAnimal, dog.ageCategory());
+        Assertions.assertEquals(oldAnimal, cat.ageCategory());
+        Assertions.assertEquals(youngAnimal, lion.ageCategory());
+        Assertions.assertEquals(babyAnimal, snake.ageCategory());
+        Assertions.assertEquals(oldAnimal, bear.ageCategory());
+    }
 }
